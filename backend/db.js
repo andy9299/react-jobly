d1a595bead84e1a6920941e0c43343c17fd82902
andy9299 <andy.tu998@gmail.com>
2023-06-23 21:00:24 -0400

    db backend setup

---
+++ backend/db.js
@@ -0,0 +1,25 @@
+"use strict";
+/** Database setup for jobly. */
+const { Client } = require("pg");
+const { getDatabaseUri } = require("./config");
+require('dotenv').config();
+
+let db;
+
+if (process.env.NODE_ENV === "production") {
+  db = new Client({
+    connectionString: getDatabaseUri(),
+    ssl: {
+      rejectUnauthorized: false
+    }
+  });
+} else {
+  db = new Client({
+    database: getDatabaseUri(),
+    password: process.env.DB_PASSWORD
+  });
+}
+
+db.connect();
+
+module.exports = db;
